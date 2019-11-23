ed3868ed594fa47c554beb34a66d31715352c86b
Toupag <wagn3rdavid@gmail.com>
2019-11-23 14:54:06 +0100

    Update TB_ShipMethod.sql

--- task 1/oltp/TB_ShipMethod.sql
+++ task 1/oltp/TB_ShipMethod.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS `BI_OLTP`.`TB_ShipMethod` (
   `Name` VARCHAR(255) NOT NULL,
   `CostPerShipment` DECIMAL(38,2) NOT NULL,
   `CostPerUnit` DECIMAL(38,2) NOT NULL,
-  `BulkyItemSurcharge` INT NOT NULL,
+  `BulkyItemSurcharge` DECIMAL(38,2) NOT NULL,
   CONSTRAINT PK_ShipMethod PRIMARY KEY (`ShipMethodID`))
 ENGINE = InnoDB
