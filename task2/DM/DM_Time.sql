24dffc43ef91550e2f89abce5987c61248e6fc41
Peter Bognár <bognarpaeter@gmail.com>
2019-11-24 16:45:06 +0100

    time hotfix

--- task2/DM/DM_Time.sql
+++ task2/DM/DM_Time.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS `BI_OLAP`.`DM_Time` (
   `Date` DATE NOT NULL,
   `DayNumberOfMonth` INT NOT NULL,
   `MonthNumberOfYear` INT NOT NULL,
-  `CalendarYear` INT NOT NULL
+  `CalendarYear` INT NOT NULL,
   CONSTRAINT PK_Date PRIMARY KEY (`Date`))
