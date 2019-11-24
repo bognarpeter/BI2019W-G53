b0a7000716e548772664e2aa87b4fcf9f356cde1
Peter Bognár <bognarpaeter@gmail.com>
2019-11-24 15:19:40 +0100

    added factsales and modified customer and product tables

--- task2/DM/DM_Customer.sql
+++ task2/DM/DM_Customer.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS `BI_OLAP`.`DM_Customer` (
   `CustomerID` INT NOT NULL,
-  `Name` VARCHAR(255) NOT NULL,
+  `Name` VARCHAR(150) NOT NULL,
   `BirthDate` DATE NOT NULL,
   `Age` INT NOT NULL,
-  `Gender` VARCHAR(15) NOT NULL,
+  `Gender` VARCHAR(10) NOT NULL,
   `Email` VARCHAR(255) NOT NULL,
   `Phone` INT NULL,
   CONSTRAINT PK_CustomerID PRIMARY KEY (`CustomerID`))
---
+++ task2/DM/DM_FactSales.sql
@@ -0,0 +1,57 @@
+CREATE TABLE IF NOT EXISTS `BI_OLAP`.`DM_Customer` (
+  `SalesOrderNumber` INT NOT NULL,
+  `SalesOrderLineNumber` INT NOT NULL,
+  `CustomerID` INT NOT NULL,
+  `ProductID` INT NOT NULL,
+  `ShipToAddressID` INT NOT NULL,
+  `BillToAddressID` INT NOT NULL,
+  `ShipmentMethod` VARCHAR(100) NOT NULL,
+  `UnitPrice` DECIMAL(10,4) NULL,
+  `Discount` DECIMAL(10,4) NULL,
+  `OrderQuantity` INT NOT NULL,
+  `OrderLineTotal` DECIMAL(10,4) NULL,
+  `OrderLineProfit` DECIMAL(10,4) NULL,
+  `TaxAmount` DECIMAL(10,4) NULL,
+  `OrderLineFreightCost` DECIMAL(10,4) NULL,
+  `OrderStatus` VARCHAR(100) NOT NULL,
+  `OrderDate` DATE NOT NULL,
+  `DueDate` DATE NOT NULL,
+  `ShipDate` DATE NOT NULL,
+  `IsLateShipment` BOOLEAN NOT NULL,
+  CONSTRAINT PK_CustomerID PRIMARY KEY (`SalesOrderNumber`, `SalesOrderLineNumber`),
+  CONSTRAINT `FK_CustomerID`
+  FOREIGN KEY (`CustomerID`)
+  REFERENCES `BI_OLAP`.`DM_Customer` (`CustomerID`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
+  CONSTRAINT `FK_ProductID`
+  FOREIGN KEY (`ProductID`)
+  REFERENCES `BI_OLAP`.`DM_Product` (`ProductID`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
+  CONSTRAINT `FK_ShipToAddressID`
+  FOREIGN KEY (`ShipToAddressID`)
+  REFERENCES `BI_OLAP`.`DM_Location` (`AddressID`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
+  CONSTRAINT `FK_BillToAddressID`
+  FOREIGN KEY (`BillToAddressID`)
+  REFERENCES `BI_OLAP`.`DM_Location` (`AddressID`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
+  CONSTRAINT `FK_OrderDate`
+  FOREIGN KEY (`OrderDate`)
+  REFERENCES `BI_OLAP`.`DM_Time` (`Date`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
+  CONSTRAINT `FK_DueDate`
+  FOREIGN KEY (`DueDate`)
+  REFERENCES `BI_OLAP`.`DM_Time` (`Date`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
+  CONSTRAINT `FK_ShipDate`
+  FOREIGN KEY (`ShipDate`)
+  REFERENCES `BI_OLAP`.`DM_Time` (`Date`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
+)
