e817468922cec67f7075c8f5d9061e0f24b8a27c
Peter Bognár <bognarpaeter@gmail.com>
2019-11-25 20:01:31 +0100

    etl customer fix

--- task2/etl/ETL_Customer.sql
+++ task2/etl/ETL_Customer.sql
@@ -2,10 +2,10 @@ INSERT INTO `BI_OLAP`.`DM_Customer`
 SElECT
   `C.CustomerID`,
   CASE WHEN `C.MiddleName` IS NOT NULL AND `C.Suffix` IS NOT NULL THEN CONCAT(`C.FirstName`, " ", `C.MiddleName`,". ",`C.LastName`,", ",`C.Suffix`)
-       WHEN `C.MiddleName` IS NOT NULL AND `C.Suffix` IS NULL THEN 1 CONCAT(`C.FirstName`, " ", `C.MiddleName`,". ",`C.LastName`)
-       WHEN `C.MiddleName` IS NULL AND `C.Suffix` IS NOT NULL THEN 1 CONCAT(`C.FirstName`, " ",`C.LastName`,", ",`C.Suffix`)
+       WHEN `C.MiddleName` IS NOT NULL AND `C.Suffix` IS NULL THEN CONCAT(`C.FirstName`, " ", `C.MiddleName`,". ",`C.LastName`)
+       WHEN `C.MiddleName` IS NULL AND `C.Suffix` IS NOT NULL THEN CONCAT(`C.FirstName`, " ",`C.LastName`,", ",`C.Suffix`)
        ELSE CONCAT(`C.FirstName`, " ", `C.LastName`)
-       END;
+  END;
   `C.BirthDate`,
   timestampdiff(YEAR,`C.BirthDate`, STR_TO_DATE("2016-01-01", "%Y-%m-%d")),
   SELECT CASE WHEN `C.Gender`='F' THEN 'Female'
