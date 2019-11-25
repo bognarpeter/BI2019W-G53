2aebbf6ff1aded1d7de51f1a1a7efbf7fe8ef094
Toupag <wagn3rdavid@gmail.com>
2019-11-25 21:44:00 +0100

    Update TB_Customer.sql

--- task 1/oltp/TB_Customer.sql
+++ task 1/oltp/TB_Customer.sql
@@ -6,8 +6,8 @@ CREATE TABLE IF NOT EXISTS `BI_OLTP`.`TB_Customer` (
   `LastName` VARCHAR(255) NOT NULL,
   `Suffix` VARCHAR(255) NULL,
   `EmailAddress` VARCHAR(255) NULL,
-  `Phone` INT NULL,
-  `Gender` VARCHAR(45) NOT NULL,
+  `Phone` VARCHAR(255) NULL,
+  `Gender` VARCHAR(255) NOT NULL,
   `Birthdate` DATE NOT NULL,
   CONSTRAINT PK_Customer PRIMARY KEY (`CustomerID`))
 ENGINE = InnoDB
