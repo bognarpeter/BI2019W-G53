edd665ed25b806606b90971a668ae3d0b0c315fc
Peter Bognár <bognarpaeter@gmail.com>
2019-11-28 22:54:53 +0100

    correct header to q1

--- task 3/sales_2.sql
+++ task 3/sales_2.sql
@@ -1,9 +1,9 @@
-SELECT L.CountryRegion as cr, sum(FS.OrderLineTotal) as sum_r
+SELECT L.CountryRegion as `Region`, sum(FS.OrderLineTotal) as `Revenue`
 FROM DM_FactSales AS FS
 JOIN DM_Location AS L
 ON FS.ShipToAddressID = L.AddressID
 JOIN DM_Time as T
 ON FS.OrderDate = T.Date
 WHERE T.CalendarYear = 2014
-GROUP BY cr
-ORDER BY cr;
+GROUP BY `Region`
+ORDER BY `Region`;
