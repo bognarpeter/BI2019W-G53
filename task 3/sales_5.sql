294c12a5bc64d44b41c7c108c9ba250a48bd5a73
Peter Bognár <bognarpaeter@gmail.com>
2019-11-28 22:55:05 +0100

    added sales/5 query

---
+++ task 3/sales_5.sql
@@ -0,0 +1,11 @@
+SELECT C.Name as `Customer Name`, sum(FS.OrderLineProfit) as `Profit`
+FROM DM_FactSales AS FS
+JOIN DM_Customer AS C
+ON FS.CustomerID = C.CustomerID
+JOIN DM_Time as T
+ON FS.OrderDate = T.Date
+WHERE T.CalendarYear = 2015
+AND T.MonthNumberOfYear > 0 AND T.MonthNumberOfYear < 7
+GROUP BY `Customer Name`
+ORDER BY `Profit` DESC
+LIMIT 10;
