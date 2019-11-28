2f7c3efe0fa13bee359b7efca0a6e91ef946bfaf
Peter Bognár <bognarpaeter@gmail.com>
2019-11-28 22:54:48 +0100

    correct header to q1

--- task 3/sales_1.sql
+++ task 3/sales_1.sql
@@ -1,9 +1,9 @@
-SELECT P.ProductTopCategory as tc, sum(FS.OrderLineProfit) as sum_p
+SELECT P.ProductTopCategory as `Top Product Category`, sum(FS.OrderLineProfit) as `Profit`
 FROM DM_FactSales AS FS
 JOIN DM_Product AS P
 ON FS.ProductID = P.ProductID
 JOIN DM_Time as T
 ON FS.OrderDate = T.Date
 WHERE T.CalendarYear = 2015
-GROUP BY tc
-ORDER BY sum_p DESC;
+GROUP BY `Top Product Category`
+ORDER BY `Profit` DESC;
