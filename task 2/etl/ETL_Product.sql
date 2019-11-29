afd3bbbb4eed62e37a342c9843238116df10d8da
Peter Bognár <bognarpaeter@gmail.com>
2019-11-29 19:21:31 +0100

    fix for sub and top categories

--- task 2/etl/ETL_Product.sql
+++ task 2/etl/ETL_Product.sql
@@ -6,14 +6,16 @@ SELECT
   P.`ProductModelName`,
   P.`StandardCost`,
   P.`ListPrice`,
-  P.`Name`,
-  PC.`Name`,
+  PC1.`Name`,
+  PC2.`Name`,
   P.`SellStartDate`,
   P.`SellEndDate`,
   P.`DiscontinuedDate`,
   P.`Size`,
   P.`Weight`,
-  IF(PC.`Name`='Bikes' OR P.`ProductCategoryID` = 16 OR P.`ProductCategoryID` = 18 OR P.`ProductCategoryID` = 20, true, false)
+  IF(PC2.`Name`='Bikes' OR P.`ProductCategoryID` = 16 OR P.`ProductCategoryID` = 18 OR P.`ProductCategoryID` = 20, true, false)
 FROM `BI_OLTP_53`.`TB_Product` AS P
-JOIN `BI_OLTP_53`.`TB_ProductCategory` AS PC
-    ON P.ProductCategoryID = PC.ProductCategoryID;
+JOIN `BI_OLTP_53`.`TB_ProductCategory` AS PC1
+    ON P.ProductCategoryID = PC1.ProductCategoryID
+JOIN `BI_OLTP_53`.`TB_ProductCategory` AS PC2
+    ON PC1.ParentProductCategoryID = PC2.ProductCategoryID;
